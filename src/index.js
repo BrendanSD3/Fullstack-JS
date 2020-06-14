77b1d03c5df1eabe29dfc6faac74a78c8be03180
bwall <brendan.wall03@gmail.com>
2020-06-14 19:44:51 +0100

    finished adding components with modules chapter 3.5 next adding state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const color = Math.random() > 0.5 ? 'green':'red';
+
+//const color = Math.random() > 0.5 ? 'green':'red';style={{ color }} <br></br>Hello React Heres a random number {Math.random()} 
+
+//import Header from './components/Header'
+import App from './components/App';
+
+
+/* App.propTypes = {
+    headerMessage: React.PropTypes.string
+  }; */
+/* Header.propTypes = {
+    message: React.PropTypes.string
+}; */
+
 ReactDOM.render(
-  <h2 className="text-center" style={{ color }}>
-    Hello React with JSX!! {Math.random()} 
-  </h2>,
+
+    <App />,
   document.getElementById('root')
 );
 //can put javascript in directly using {} the math.random function is JS
