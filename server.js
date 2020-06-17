c2db376e5cc6b05eb958db307d74ccdae9271039
bwall <brendan.wall03@gmail.com>
2020-06-17 19:26:00 +0100

    Posting New name with API works end of 7.6

--- server.js
+++ server.js
@@ -52,10 +52,13 @@ import config from './config';
 import apiRouter from './api';
 import sassMiddleware from 'node-sass-middleware';
 import path from 'path';
+import bodyParser from 'body-parser';
+
+
 
 import express from 'express';
 const server = express();
-
+server.use(bodyParser.json());
 server.use(sassMiddleware({
     src: path.join(__dirname, 'sass'),
     dest: path.join(__dirname, 'public')
